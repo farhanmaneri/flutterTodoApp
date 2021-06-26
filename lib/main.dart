879f4d9d0d346d9aa13db38b567b8594c1b368bd
farhanmaneri <farhanmaneri@gmail.com>
2021-06-25 20:50:45 -0700

    'todoappcreated'

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:todo/todo.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(     
+      home: Scaffold(
+        body: Todo(),
+      ),
+    );
+  }
+}
+
+ 
